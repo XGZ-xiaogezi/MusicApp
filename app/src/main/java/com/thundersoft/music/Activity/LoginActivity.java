0ee0b122744983e914b16f0ec49caf0730194c59
XGZ-xiaogezi <2570079002@qq.com>
2022-05-27 15:09:07 +0800

    test

---
+++ app/src/main/java/com/thundersoft/music/Activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.thundersoft.music.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.thundersoft.music.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
